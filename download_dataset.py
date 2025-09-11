cb09cf875c2fab51bbf29f192b50634e371057f0
ritiha-05 <dharaniritihamahalingam@gmail.com>
2025-09-11 12:13:38 +0530

    Update download_dataset.py

--- download_dataset.py
+++ download_dataset.py
@@ -2,7 +2,8 @@ from roboflow import Roboflow
 
 rf = Roboflow(api_key="B8WYgL8xxOoBCk30IuW7")
 project = rf.workspace("miniproject-qspoe").project("shopping-trolley-kn5tj-nisex")
-dataset = project.version(1).download("yolov8")
+dataset = project.version(2).download("yolov8")
+
 
 
 
